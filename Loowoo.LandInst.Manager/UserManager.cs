773ddc73b323f3bb8c88a30d7503103e912e6a48
Liangjun Zheng <zhengliangjun@gmail.com>
2014-04-23 10:41:35 +0800

    Create

---
+++ Loowoo.LandInst.Manager/UserManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Loowoo.LandInst.Model;
+
+namespace Loowoo.LandInst.Manager
+{
+    public class UserManager
+    {
+        public void Save(User user)
+        { 
+            
+        }
+    }
+}
