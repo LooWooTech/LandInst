2480e21c416985b870935f00117cb22edfd61106
Liangjun Zheng <zhengliangjun@gmail.com>
2014-05-09 19:03:03 +0800

    add miss file

---
+++ Loowoo.LandInst.Model/VApprovalEducation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+
+namespace Loowoo.LandInst.Model
+{
+    [Table("VApproval_Education")]
+    public class VApprovalEducation : VApprovalBase
+    {
+        public int EduID { get; set; }
+
+        public string EduName { get; set; }
+
+        public string RealName { get; set; }
+    }
+}
