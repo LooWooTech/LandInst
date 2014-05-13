152faa05621e2d2b908a3ce5c02d07e57027a821
Liangjun Zheng <zhengliangjun@gmail.com>
2014-05-13 11:30:24 +0800

    add miss file

---
+++ Loowoo.LandInst.Manager/AnnualCheckManager.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Loowoo.LandInst.Model;
+using Loowoo.LandInst.Model.Filters;
+
+namespace Loowoo.LandInst.Manager
+{
+    public class AnnualCheckManager : ManagerBase
+    {
+        public void Save(AnnualCheck model)
+        {
+            using (var db = GetDataContext())
+            {
+                if (model.ID > 0)
+                {
+                    var entity = db.AnnualChecks.FirstOrDefault(e => e.ID == model.ID);
+                    if (entity != null)
+                    {
+                        db.Entry(entity).CurrentValues.SetValues(model);
+                    }
+                }
+                else
+                {
+                    db.AnnualChecks.Add(model);
+                }
+                db.SaveChanges();
+            }
+        }
+
+        public List<VInstAnnualCheck> GetInstAnnualChecks(int instId)
+        {
+            using (var db = GetDataContext())
+            {
+                return db.VInstAnnualChecks.Where(e => (e.ApprovalType == ApprovalType.Annual && e.UserID == instId) || e.UserID == null).ToList();
+            }
+        }
+
+        public List<VApprovalAnnualCheck> GetApprovalAnnualChecks(ApprovalFilter filter)
+        {
+            using (var db = GetDataContext())
+            {
+                var query = db.VApprovalAnnualChecks.Where(e => e.ApprovalType == filter.Type);
+                if (!string.IsNullOrEmpty(filter.Keyword))
+                {
+                    query = query.Where(e => e.Fullname.Contains(filter.Keyword) || e.InstName.Contains(filter.Keyword));
+                }
+
+                if (filter.InfoID.HasValue)
+                {
+                    query = query.Where(e => e.InfoID == filter.InfoID.Value);
+                }
+
+                return query.OrderByDescending(e => e.CreateTime).SetPage(filter).ToList();
+            }
+        }
+
+        public List<AnnualCheck> GetAnnualChecks()
+        {
+            using (var db = GetDataContext())
+            {
+                return db.AnnualChecks.OrderByDescending(e => e.ID).ToList();
+            }
+        }
+
+        public AnnualCheck GetModel(int annualCheckId)
+        {
+            using (var db = GetDataContext())
+            {
+                return db.AnnualChecks.FirstOrDefault(e => e.ID == annualCheckId);
+            }
+        }
+    }
+}
---
+++ Loowoo.LandInst.Model/AnnualCheck.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loowoo.LandInst.Model
+{
+    [Table("AnnualCheck")]
+    public class AnnualCheck
+    {
+        [Key]
+        [DatabaseGenerated(System.ComponentModel.DataAnnotations.Schema.DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        public string Name { get; set; }
+
+        public string Summary { get; set; }
+    }
+}
